55532c65d3c9f469588ce16453ffe9c997e4794c
omkarmm <omkarmm11@gmail.com>
2024-08-28 16:33:10 +0530

    Formatted Codes

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -65,7 +65,7 @@ export const getJobById = async (req, res) => {
     try {
         const jobId = req.params.id;
         const job = await Job.findById(jobId).populate({
-            path:"applications"
+            path: "applications"
         });
         if (!job) {
             return res.status(404).json({
@@ -83,8 +83,8 @@ export const getAdminJobs = async (req, res) => {
     try {
         const adminId = req.id;
         const jobs = await Job.find({ created_by: adminId }).populate({
-            path:'company',
-            createdAt:-1
+            path: 'company',
+            createdAt: -1
         });
         if (!jobs) {
             return res.status(404).json({
